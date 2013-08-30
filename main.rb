d9ebc6d94c77590498fb0ba7b7c502193571c815
Gabe <gabedominguez@outlook.com>
2013-08-29 21:05:53 -0700

    set up hanoi with a play and home button

--- main.rb
+++ main.rb
@@ -3,14 +3,6 @@ require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'haml'
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter => 'postgresql',
-#   :host => 'localhost',
-#   :username => 'Gabriel',
-#   :password => '',
-#   :encoding => 'utf8'
-#   )
-
 get "/" do
   haml :index, :format => :html5
 end
---
+++ public/js/hanoi.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  var peg1 = $('#peg1')
+  var peg2 = $('#peg2')
+  var peg3 = $('#peg3')
+
+  function play(){
+    var discnum = prompt("How many discs?")
+    var counter = 0
+
+    function setup(){
+      if (counter < discnum){
+        counter++
+        peg1.append('<div class="disc" id='+counter+'>'+counter+'</div>');
+        setup()
+      }
+    }
+    setup()
+
+    $(function movedisc() {
+        $( ".peg div:first-child" ).draggable();
+        $( ".peg" ).droppable({
+          drop: function( event, ui ) {
+            var currentdisc = $('.ui-draggable-dragging')
+            if ($(this).find("div:first-child").length === 0 || $(this).find("div:first-child").attr('id')  > currentdisc.attr('id')){
+              $(this).prepend(currentdisc.removeAttr('style'))
+              $(this).children().draggable().draggable('destroy');
+              $( ".peg div:first-child" ).draggable();
+              if ($('#peg3').find('div').length == discnum){
+                alert('You win!');
+              }
+            } else {
+              alert("you can't move there!")
+              currentdisc.removeAttr('style')
+              movedisc()
+            }
+          }
+        });
+      });
+  }
+
+  $('#playbutton').on('click', function(){
+    play();
+  });
+});
